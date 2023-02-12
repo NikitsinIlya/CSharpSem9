55a90c3fbe357767ed16c0a18a0c33a7b0169d97
NikitsinIlya <JamsN87@gmail.com>
2023-02-12 14:28:24 +0300

    Final HomeWork

--- DomZadanie/Program.cs
+++ DomZadanie/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Task 66
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+int f(int m, int n )
+{
+    if (m == n)
+        return n;
+    return f (m, n - 1) + n;
+}
+
+Console.Clear();
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Сумма натуральных элементов : {f(m, n)}");
+*/
+
+// Task 68
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int f(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m != 0 && n == 0)
+        return f(m - 1, 1);
+    else
+        return f(m - 1, f(m, n - 1));
+}
+
+Console.Clear();
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(f(m, n));
